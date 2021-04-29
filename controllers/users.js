bb92a01ec340f78cb1650142fb6fa867f59f72db
AntoninaP <antonina.filippa@gmail.com>
2021-04-29 13:14:39 +0300

    start project 14

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,9 @@
+const bcrypt = require('bcryptjs');
 const User = require('../models/user');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
 
-const opt = { new: true, runValidators: true };
+const opt = {new: true, runValidators: true};
 
 const getUsers = async (req, res) => {
   try {
@@ -9,9 +12,9 @@ const getUsers = async (req, res) => {
     res.status(200).send(allUsers);
   } catch (err) {
     if (err.message === 'NotValid') {
-      res.status(404).send({ message: 'запрашиваемые пользователи не найдены' });
+      res.status(404).send({message: 'запрашиваемые пользователи не найдены'});
     } else {
-      res.status(500).send({ message: 'На сервере произошла ошибка' });
+      res.status(500).send({message: 'На сервере произошла ошибка'});
     }
   }
 };
@@ -23,63 +26,100 @@ const getUserById = async (req, res) => {
     res.status(200).send(userWithId);
   } catch (err) {
     if (err.name === 'CastError') {
-      res.status(400).send({ message: 'Переданы некорректные данные' });
+      res.status(400).send({message: 'Переданы некорректные данные'});
     } else if (err.message === 'NotValidId') {
-      res.status(404).send({ message: 'Объект не найден' });
+      res.status(404).send({message: 'Объект не найден'});
     } else {
-      res.status(500).send({ message: 'На сервере произошла ошибка' });
+      res.status(500).send({message: 'На сервере произошла ошибка'});
     }
   }
 };
 
 const createUser = async (req, res) => {
-  const { name, about, avatar } = req.body;
+  const {name, about, avatar, email, password} = req.body;
+  validator.isEmail(email);
   try {
-    const user = await User.create({ name, about, avatar });
-    res.send({ data: user });
+    // хешируем пароль
+    const hash = await bcrypt.hash(password, 10)
+    const user = await User.create({name, about, avatar, email, hash});
+    res.send({data: user});
   } catch (err) {
     if (err.name === 'ValidationError') {
-      res.status(400).send({ message: err.message });
+      res.status(400).send({message: err.message});
     } else {
-      res.status(500).send({ message: 'На сервере произошла ошибка' });
+      res.status(500).send({message: 'На сервере произошла ошибка'});
     }
   }
 };
 
 const updateProfile = async (req, res) => {
-  const { name, about } = req.body;
+  const {name, about} = req.body;
   try {
-    const newUser = await User.findByIdAndUpdate(req.user._id, { name, about }, opt)
+    const newUser = await User.findByIdAndUpdate(req.user._id, {name, about}, opt)
       .orFail(new Error('NotValidId'));
     res.status(200).send(newUser);
   } catch (err) {
     if (err.name === 'ValidationError') {
-      res.status(400).send({ message: 'Переданы некорректные данные' });
+      res.status(400).send({message: 'Переданы некорректные данные'});
     } else if (err.message === 'NotValidId') {
-      res.status(404).send({ message: 'Объект не найден' });
+      res.status(404).send({message: 'Объект не найден'});
     } else {
-      res.status(500).send({ message: 'На сервере произошла ошибка' });
+      res.status(500).send({message: 'На сервере произошла ошибка'});
     }
   }
 };
 
 const updateAvatar = async (req, res) => {
-  const { avatar } = req.body;
+  const {avatar} = req.body;
   try {
-    const newAvatar = await User.findByIdAndUpdate(req.user._id, { avatar }, opt)
+    const newAvatar = await User.findByIdAndUpdate(req.user._id, {avatar}, opt)
       .orFail(new Error('NotValidId'));
     res.status(200).send(newAvatar);
   } catch (err) {
     if (err.name === 'ValidationError') {
-      res.status(400).send({ message: 'Переданы некорректные данные' });
+      res.status(400).send({message: 'Переданы некорректные данные'});
     } else if (err.message === 'NotValidId') {
-      res.status(404).send({ message: 'Объект не найден' });
+      res.status(404).send({message: 'Объект не найден'});
     } else {
-      res.status(500).send({ message: 'На сервере произошла ошибка' });
+      res.status(500).send({message: 'На сервере произошла ошибка'});
     }
   }
 };
 
+const login = (req, res) => {
+  const {email, password} = req.body;
+  try {
+    User.findOne({email})
+      .then((user) => {
+        if (!user) {
+          return Promise.reject(new Error('Неправильные почта или пароль'));
+        }
+        return bcrypt.compare(password, user.password);
+      })
+      .then((matched) => {
+        if (!matched) {
+          // хеши не совпали — отклоняем промис
+          return Promise.reject(new Error('Неправильные почта или пароль'));
+        }
+        // совпали - аутентификация успешна
+        res.send({message: 'Всё верно!'});
+      })
+
+    // создадим токен
+    const token = jwt.sign({_id: user._id},
+      'some-secret-key',
+      {expiresIn: '7d'});
+    // вернём токен
+    res.send({token});
+
+  } catch (err) {
+    res
+      .status(401)
+      .send({message: err.message});
+  }
+};
+
+
 module.exports = {
-  getUsers, getUserById, createUser, updateProfile, updateAvatar,
+  getUsers, getUserById, createUser, updateProfile, updateAvatar, login
 };
--- models/user.js
+++ models/user.js
@@ -1,22 +1,33 @@
 const mongoose = require('mongoose');
+
 // схема для пользователя
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
+    default: 'Жак-Ив Кусто',
     minlength: [2, 'Минимальная длина 2 символа'],
     maxlength: [30, 'Максимальная длина 30 символов'],
   },
   about: {
     type: String,
-    required: true,
+    default: 'Исследователь',
     minlength: [2, 'Минимальная длина 2 символа'],
     maxlength: [30, 'Максимальная длина 30 символов'],
   },
   avatar: {
     type: String,
-    required: true,
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
   },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8
+  }
 });
 
 module.exports = mongoose.model('user', userSchema);
